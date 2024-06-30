932d855d3586bc522a88de792aa8bd35da001558
o joey <joey90125@gmail.com>
2024-06-30 21:45:07 +0900

    python入門課題完了しました。

--- pi.py
+++ pi.py
@@ -2,6 +2,18 @@ text = """
     How I want a drink, alcoholic of course, after the heavy chapters involving
     quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
 """
-
+ 
 # TODO
 
+
+words = text.split() #splitって何？
+print(words)    #この状態でリストになっている
+
+lengh_list = list(map(len,words))
+Ans = "".join(map(str, lengh_list))    
+print(Ans)       #mapあり
+
+A = ""
+for i in words:
+    A += str(len(i))
+print(A)        #mapなし
