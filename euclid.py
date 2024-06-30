65bfe1c1fae1fdd063b9ec7d81c1125c8b900094
o joey <joey90125@gmail.com>
2024-06-30 22:25:24 +0900

    制御構文課題完了しました

--- euclid.py
+++ euclid.py
@@ -1,4 +1,45 @@
-a = input("a の値を入力: ")
-b = input("b の値を入力: ")
+#a = input("a の値を入力: ")
+#b = input("b の値を入力: ")
 
 # TODO
+
+#---------------------------
+a = 629     #試作
+b = 259
+while True:
+    r = a%b
+    a = b
+    b = r
+    print(b)
+
+    if r == 0:
+         break
+
+#------------------------------------------
+def euclid(a, b):    #本番（課題用）
+    while not b == 0:
+        if a < b:
+         a,b = b,a
+
+    a, b = b, a % b
+    return a
+    
+
+
+result = euclid(20, 10)
+result1 = euclid(14,91)
+result2 = euclid(91,14)
+print(result)
+print(result1)
+print(result2)
+
+
+
+
+#------------------------------------------------------------------
+
+
+
+
+
+     
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,3 +3,19 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
+
+
+#制御構文の課題
+from math import pi
+
+a =  pi / 2
+b = 0
+N = 100
+h = (b-a)/N
+S = 0
+for k in range(1,N+1):
+   S += (h/2)*(sin(a+(k-1)*h)+sin(a+k*h))
+
+print(abs(S))
+
+
