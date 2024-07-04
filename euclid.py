2a381d158f0f4696b3d6cc58876222b6a64a31ab
o joey <joey90125@gmail.com>
2024-07-04 21:44:52 +0900

    制御構文課題の修正完了しました

--- euclid.py
+++ euclid.py
@@ -4,31 +4,20 @@
 # TODO
 
 #---------------------------
-a = 629     #試作
-b = 259
-while True:
-    r = a%b
-    a = b
-    b = r
-    print(b)
 
-    if r == 0:
-         break
 
 #------------------------------------------
 def euclid(a, b):    #本番（課題用）
-    while not b == 0:
-        if a < b:
-         a,b = b,a
-
-    a, b = b, a % b
+    while b != 0:
+     a, b = b, a % b
     return a
     
 
 
-result = euclid(20, 10)
+result = euclid(20,10)
 result1 = euclid(14,91)
 result2 = euclid(91,14)
+
 print(result)
 print(result1)
 print(result2)
