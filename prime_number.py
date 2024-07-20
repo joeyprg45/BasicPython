8acabffe6ce8ca5ff80f577c359672a240219e0a
o joey <joey90125@gmail.com>
2024-07-21 01:51:50 +0900

    関数課題の修正２回目終わりました。

--- prime_number.py
+++ prime_number.py
@@ -5,23 +5,24 @@
 
 
 def sosuu(n):
- if type(n) == float:
-   return "error"
- elif n < 1:
-   return "error"
- elif n == 1:
-   return False
- elif n > 1:
-   for i in range(2, n):
-        if n % i == 0:
-         return False
-        return True
+    if not isinstance(n, int):
+        raise TypeError("int型以外の入力は受け付けません")
+    elif n < 1:
+        raise ValueError("入力は自然数である必要があります")
+    elif n == 1:
+        return False
+    elif n > 1:
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+    return True
 
 print(sosuu(61))
 print(sosuu(10))
 print(sosuu(1))
 print(sosuu(-5))
 print(sosuu(3.14))
-print(sosuu(1))
+print(sosuu(13))
+
 
 print(type(sosuu(61)))
